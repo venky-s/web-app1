eb005306d22555968b77720ad2b66cc3df83f915
Venky-S <strivenky@gmail.com>
2021-07-18 19:16:19 +0530

    follow-up page design

--- src/app/agency/prospect/components/registered-prospect/registered-prospect.component.ts
+++ src/app/agency/prospect/components/registered-prospect/registered-prospect.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registered-prospect',
@@ -7,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisteredProspectComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
+  newProspectBtn(){    
+      this.router.navigate(['/agency/my-qr']);
+  }
   ngOnInit(): void {
   }
 
